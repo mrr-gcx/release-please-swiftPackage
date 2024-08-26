8c5768acd276f4f714015776f3984df071ebd6cb
Marcos Ramos Rubio <66588987+mrr-gcx@users.noreply.github.com>
2024-08-26 14:49:17 +0200

    feat: adds swift package strategy
    - Adds swift-package strategy
    - Adds package-version updater
    - Updates cli strategy options
    - Adds SwiftPackage strategy to factory.ts

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let version = "16.12.0"
+
+let package = Package(
+    name: "DeckOfPlayingCards",
+    products: [
+        .library(name: "DeckOfPlayingCards", targets: ["DeckOfPlayingCards"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/example-package-fisheryates.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/example-package-playingcard.git", from: "3.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "DeckOfPlayingCards",
+            dependencies: ["FisherYates", "PlayingCard"]),
+        .testTarget(
+            name: "DeckOfPlayingCardsTests",
+            dependencies: ["DeckOfPlayingCards"]),
+    ]
+)
